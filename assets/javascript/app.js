2837a004ad4abd2f785f6a9f7261de1b9ab58be9
Onray <mcintyrehh@gmail.com>
2018-07-15 23:04:35 -0400

    favorites saving to an array now, button coming next!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function () {
     var queryURL;
+    var favoriteGifs = [];
     var gifArray = ["Snoop Dogg", "Nas", "Wu-Tang", "Biggie", "Bone Thugz", "Gucci Mane", "Lil Wayne", "Tupac", "Kanye", "Tyler the Creator", "Busta Rhymes", "Lil Pump", "RiFF RAFF", "Action Bronson", "Bird Man"];
     var gifArraySearch = ["Snoop Dogg", "Nas", "Wu-Tang", "Biggie", "Bone Thugz", "Gucci Mane", "Lil Wayne", "Tupac", "Kanye", "Tyler the Creator", "Busta Rhymes", "Lil Pump", "RiFF RAFF", "Action Bronson", "Bird Man"];
     var x = 10;
@@ -13,7 +14,7 @@ $(document).ready(function () {
             }
         }
     }
-//toggle for additive gifs, or for fresh ones each time a button is pressed
+    //toggle for additive gifs, or for fresh ones each time a button is pressed
     var toggleAddFresh = "fresh";
     $('.add').on("click", function () {
         toggleAddFresh = "add";
@@ -35,18 +36,29 @@ $(document).ready(function () {
         $('.buttons').append("<button class=\"btn btn-primary gen-button\" type=\"button\" id=\"button_" + r + "\">" + gifArray[r] + "</div>");
         $('#button_' + r).data("name", val);
     }
-    $('#add').on("click", function() {
-        var newButton = $('#typedtext').val();
+    $('#add').on("click", function () {
+        var newButton = $('#typedtext').val().trim();
         gifArray.push(newButton);
         var newIndex = gifArray.indexOf(newButton);
+        var newName = gifArray[newIndex];
+        console.log(newIndex);
         $('.buttons').append("<button class=\"btn btn-primary gen-button\" type=\"button\" id=\"button_" + newIndex + "\">" + gifArray[newIndex] + "</div>");
-        $('#button_' + newIndex).data("name", newButton);
-        console.log($(this).data("name"));
+        $('#button_' + newIndex).data("name", newName);
+        console.log($('#button_15').data("name"));
+        $('#button_' + newIndex).on("click", function () {
+            onClickGifs($(this));
+        });
     });
     $('.gen-button').on("click", function () {
-        // var array;
-        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).data("name") + "&api_key=s9kLWgDVn7rUQdMDmUSknpwBTvdwPrzT&limit=50";
-        console.log($(this).data("name"));
+        x = 10;
+        console.log(x);
+        onClickGifs($(this));
+    });
+
+    function onClickGifs(gif) {
+        // var buttonClicked = 
+        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif.data("name") + "&api_key=s9kLWgDVn7rUQdMDmUSknpwBTvdwPrzT&limit=50";
+        console.log(gif.data("name"));
         x = 10;
         $.ajax({
             url: queryURL,
@@ -73,24 +85,19 @@ $(document).ready(function () {
                 gifCard.append(b);
                 var overlay = $("<div>");
                 overlay.addClass("card-img-overlay d-flex flex-column justify-content-end hover-text");
-                overlay.append('<h5 class="card-title gif-name hide">' + array.data[x].title + '</h5>' + '<div class="fav-div"><i class="fas fa-star fav btn hide"></i></div>' +
+                overlay.append('<h5 class="card-title gif-name hide">' + array.data[x].title + '</h5>' + '<span class="fav-div" fav-saved="no"><i class="far fa-star mx-auto fav btn hide"></i></div>' +
                     '<button class="btn dl-btn hide"><i class="fa fa-download"></i> Download</button>');
                 gifCard.append(overlay);
-                $(".fav").on("click", function () {
-                    console.log("yup");
-                    $(".fav-div").empty();
-                    $(".fav-div").html('<i class="fas fa-star fav btn hide"></i>');
-
-                });
+               
                 $(".gifs").append(gifCard);
                 //elements with the hide class will only be shown on hover
                 $('.hide').hide();
+                hoverLogic(gifCard);
+
+
             }
             if (toggleAddFresh === "fresh") {
                 $(".gifs").empty();
-                // array.data.forEach(function (element) {
-                //     return addGifs(element);
-                // });
                 $('.more').html('<div class="col-md-2 btn btn-success mx-auto d-flex justify-content-center more">Load More</div>');
 
                 for (i = (x - 10); i < x; i++) {
@@ -106,42 +113,60 @@ $(document).ready(function () {
                 });
             }
             else {
-                // array.data.forEach(function (element) {
-                //     return addGifs(element);
-                // })
                 $('.more').html('<div class="col-md-2 btn btn-success mx-auto d-flex justify-content-center more">Load More</div>');
 
                 for (j = (x - 10); j < x; j++) {
                     addGifs(j);
+
                 }
             }
-
-            $(".card").hover(
-                function () {
-                    $(this).find(".gif").css("opacity", "0.2");
-                    $(this).find(".hide").show();
-                }, function () {
-                    $(this).find(".gif").css("opacity", "1");
-                    $(this).find(".hide").hide();
-
-                }
-            );
-
-            $(".card").on("click", function () {
-                var state = $(this).find(".gif").attr("data-state");
-                var img = $(this).find('.gif');
-                if (state === 'still') {
-                    img.attr('src', img.attr('data-gif'));
-                    img.attr('data-state', 'animate');
-                }
-                else if (state === 'animate') {
-                    img.attr('src', img.attr('data-static'));
-                    img.attr('data-state', 'still');
-                }
-            });
+            // hoverLogic('.card');
         });
-    });
 
+    }
+    
+        $(".gifs").on("click", ".fav-div", function () {
+            if ($(this).attr("fav-saved") === "no") {
+                $(this).attr("fav-saved", "yes");
+                $(this).empty().html('<i class="fas fa-star mx-auto fav btn hide"></i><button class="btn dl-btn hide"><i class="fa fa-download"></i> Download</button>');
+                var newFav = $(this).parent().parent().html();
+                console.log(newFav);
+                favoriteGifs.push(newFav);
+                console.log(favoriteGifs[0]);
+            }
+            else if ($(this).attr("fav-saved") === "yes") {
+                $(this).attr("fav-saved", "no");
+                $(this).empty().html('<i class="far fa-star mx-auto fav btn hide"></i><button class="btn dl-btn hide"><i class="fa fa-download"></i> Download</button>');
+                console.log($(this).attr("fav-saved"));
+            }
+
+        });
+    
+  
+    function hoverLogic(hover) {
+        $(hover).hover(
+            function () {
+                $(this).find(".gif").css("opacity", "0.2");
+                $(this).find(".hide").show();
+            }, function () {
+                $(this).find(".gif").css("opacity", "1");
+                $(this).find(".hide").hide();
+
+            }
+        );
+        $(hover).on("click", function () {
+            var state = $(this).find(".gif").attr("data-state");
+            var img = $(this).find('.gif');
+            if (state === 'still') {
+                img.attr('src', img.attr('data-gif'));
+                img.attr('data-state', 'animate');
+            }
+            else if (state === 'animate') {
+                img.attr('src', img.attr('data-static'));
+                img.attr('data-state', 'still');
+            }
+        });
+    }
 });
 
 
